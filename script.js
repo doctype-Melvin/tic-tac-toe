f25539abb4ee70094314bb1a539a3cff6c8546e9
doctype-Melvin <doctype.melvin@gmail.com>
2022-04-25 07:00:02 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const MyApp = {
+    scale: function(){
+    console.log(`I popped up here!`)
+},
+    doubleScale: function(){
+    console.log(`Double up!`)
+    }
+};
+
+function makeBread(){
+    return `Heat it up`
+}
