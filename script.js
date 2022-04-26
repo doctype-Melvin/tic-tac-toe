b2d73a201a8d81c88d96fbb380e5e9a4cdb15781
doctype-Melvin <doctype.melvin@gmail.com>
2022-04-26 11:21:04 +0200

    adds oneway marks to grid

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const TicTacToe = () => {
-    const GAMEBOARD = (() => {
-        let gameBoard = []
-        return gameBoard
-    })();
-
-    const PLAYER = (name, shape) => {
-        let score = 0;
-        let getName = () => name;
-        let getShape = () => shape;
+const gameboard = (() => {
+    let array = [];
+    const fields = [...document.querySelectorAll('.field')];
+    fields.forEach(field => field.addEventListener('click',
+    (e) => array.push(parseInt(e.target.dataset.field))));
+    return {
+        array: array,
+        fields: fields
     }
-}
+})();
+
+const drawShape = (() => {
+    let fieldText = gameboard.fields.textContent;
+    const shapes = ['x', 'o'];
+    gameboard.fields.forEach(field => field.addEventListener('click', () => {
+        if(field.textContent === '') field.textContent = shapes[0];
+        if(field.textContent === shapes[0] || field.textContent === shapes[1]) return
+    }))
+})();
