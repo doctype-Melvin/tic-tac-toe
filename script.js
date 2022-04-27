f8814eca69213f98c25714cdbac0e131c3ffdf84
doctype-Melvin <doctype.melvin@gmail.com>
2022-04-27 11:58:05 +0200

    adds fn that alternates between markers

--- script.js
+++ script.js
@@ -11,9 +11,22 @@ const gameBoard = (() => {
     }
 })();
 
-const Marker = (string) => {
-    const marker = string;
+const Player = (string) => {
+    const getMarker = () => string;
     return {
-        marker
+        getMarker
     }
 }
+
+const player1 = Player('x').getMarker();
+const player2 = Player('o').getMarker();
+
+function placeMarker(player){
+    gameBoard.grid.forEach(field=>field.addEventListener('click', (e) => {
+        e.target.textContent = player;
+        if(player === 'x') placeMarker(player2);
+        if(player === 'o') placeMarker(player1);
+    }))
+}
+
+placeMarker(player1)
